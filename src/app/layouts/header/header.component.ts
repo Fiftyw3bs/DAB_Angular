334f25c987e3982a2bff426b708cdf97c400daf0
Aqueel Bahoo <mr.aqeelbahoo@gmail.com>
2021-01-16 23:12:33 +0500

    -Added tpye on header

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { HelperService } from './../../services/helper.service';
 
@@ -7,28 +7,25 @@ import { HelperService } from './../../services/helper.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
-  logged_in: Boolean = false;
-  language: String = 'English';
-  user_role: String;
+export class HeaderComponent {
+  public logged_in: Boolean = false;
+  public user_role: String;
 
   constructor(private router: Router, private helperService: HelperService) {
     this.helperService.isLoggedIn.subscribe((res) => {
       this.logged_in = res;
     });
   }
-  ngOnInit() {}
 
   ngDoCheck() {
     this.user_role = sessionStorage.getItem('role');
-
     const user_session_id = sessionStorage.getItem('user_session_id');
     if (user_session_id) {
       this.logged_in = true;
     }
   }
 
-  logOut() {
+  public logOut() {
     sessionStorage.clear();
     this.helperService.isLoggedIn.next(false);
     this.router.navigateByUrl('/login');
